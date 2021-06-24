753584222cda1266f9f94a17fa7224c8ac142098
Shakir Hussain <shakir.hussain@siyatechventures.com>
2021-06-24 12:18:13 +0530

    Initilize Pub

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,6 +1,9 @@
-struct MyLibrary {
+public struct MyLibrary {
     var text = "Hello, World!"
     
+    public init(){
+        
+    }
     
     public func helloWorldPrint()->String {
         return "Hello, World!"
